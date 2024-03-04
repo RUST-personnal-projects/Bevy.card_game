dabf80e29cf98db3a0a1c462266dbda0006fb9ad
VALENTIN ROUSSEAU <59ichiigo@gmail.com>
2024-03-04 19:49:14 +0100

    init first app

---
+++ card_game/src/main.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::default()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
