b5c900dec521793789c7868788677116d2604ba3
VALENTIN ROUSSEAU <59ichiigo@gmail.com>
2024-03-20 18:50:40 +0100

    add (WIP) test to hover plugin

--- card_game/src/utils/mouse/hover.rs
+++ card_game/src/utils/mouse/hover.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use super::{click::Clicked, MouseCoordinates};
+use super::{Clicked, MouseCoordinates};
 
 #[derive(Component, Debug, Default)]
 pub struct Hoverable;
@@ -10,15 +10,7 @@ pub struct Hovered;
 
 pub struct HoverPlugin;
 
-#[derive(Resource, Debug)]
-pub struct GizmoColor(pub Color);
-
-impl Default for GizmoColor {
-    fn default() -> Self {
-        Self(Color::GREEN)
-    }
-}
-
+/// This plugins allows the App to know an Hoverable entity with an Image Component is being hovered by the mouse
 impl Plugin for HoverPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, (is_hovered, gizmo));
@@ -70,4 +62,54 @@ fn is_hovered(
             commands.entity(entity).remove::<Hovered>();
         }
     }
+}
+
+#[cfg(test)] // This attribute ensures this module is only compiled when running tests
+mod hover {
+    // Import the testing module
+    use super::*;
+    // mod is_hover {
+    //     use bevy::{asset::io::AssetSources, tasks::IoTaskPool};
+
+    //     use super::*;
+
+    //     #[test]
+    //     // Hoverable [V] Hovering [V]
+    //     fn hoverable_hovering() {
+    //         // Setup app
+    //         let mut app = App::new();
+    //         app.add_plugins(AssetPlugin::default());
+
+    //         app.init_resource::<MouseCoordinates>();
+    //         app.init_resource::<Assets<Image>>();
+
+    //         app.init_asset::<bevy::render::texture::Image>();
+
+    //         let mut coordinates = app.world.resource_mut::<MouseCoordinates>();
+    //         coordinates.0 = Vec2::new(0., 0.);
+
+    //         // Access the asset server
+    //         let asset = app.world.get_resource::<Asset<Image>>().unwrap();
+
+    //         let image: Handle<Image> = asset_server.load("cards/card_back/card_back.png");
+
+    //         // Add Clickable entity that is also Hovered
+    //         let entity_id = app
+    //             .world
+    //             .spawn((Hoverable, image, Transform::from_xyz(0., 0., 0.)))
+    //             .id();
+
+    //         // Add our system
+    //         app.add_systems(Update, is_hovered);
+
+    //         // update the game once to run the system
+    //         app.update();
+
+    //         // retrieve entity after update
+    //         let entity = app.world.get_entity(entity_id);
+
+    //         assert!(entity.is_some());
+    //         assert!(entity.unwrap().contains::<Hovered>());
+    //     }
+    // }
 }
