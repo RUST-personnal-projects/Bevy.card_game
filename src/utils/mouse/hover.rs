6410992ecc3a02c75a6723ff43afed65a79d87f2
VALENTIN ROUSSEAU <59ichiigo@gmail.com>
2024-04-15 18:08:46 +0200

    add protection to unwrap

--- src/utils/mouse/hover.rs
+++ src/utils/mouse/hover.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{asset::LoadState, prelude::*};
 
 use super::{Clicked, MouseCoordinates};
 
@@ -42,22 +42,28 @@ fn is_hovered(
         (Entity, &Handle<Image>, &Transform),
         (With<Hoverable>, Without<Clicked>),
     >,
-    assets: Res<Assets<Image>>,
+    images: Res<Assets<Image>>,
+    assets: Res<AssetServer>,
     mouse: Res<MouseCoordinates>,
     mut commands: Commands,
 ) {
     for (entity, image, transform) in hoverables_query.iter() {
-        let image = assets.get(image).unwrap();
-        let half_width = image.width() as f32 / 2.;
-        let half_height = image.height() as f32 / 2.;
+        if assets.load_state(image) == LoadState::Loaded {
+            let image = images.get(image).unwrap();
+            let half_width = image.width() as f32 / 2.;
+            let half_height = image.height() as f32 / 2.;
 
-        let min_x = transform.translation.x - half_width;
-        let max_x = transform.translation.x + half_width;
-        let min_y = transform.translation.y - half_height;
-        let max_y = transform.translation.y + half_height;
+            let min_x = transform.translation.x - half_width;
+            let max_x = transform.translation.x + half_width;
+            let min_y = transform.translation.y - half_height;
+            let max_y = transform.translation.y + half_height;
 
-        if mouse.0.x >= min_x && mouse.0.x <= max_x && mouse.0.y >= min_y && mouse.0.y <= max_y {
-            commands.entity(entity).insert(Hovered);
+            if mouse.0.x >= min_x && mouse.0.x <= max_x && mouse.0.y >= min_y && mouse.0.y <= max_y
+            {
+                commands.entity(entity).insert(Hovered);
+            } else {
+                commands.entity(entity).remove::<Hovered>();
+            }
         } else {
             commands.entity(entity).remove::<Hovered>();
         }
